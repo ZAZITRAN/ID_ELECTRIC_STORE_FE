3429d44d33db4ed75f3fff14b6df907db8a45f80
LeDuc2004 <114064159+LeDuc2004@users.noreply.github.com>
2023-03-20 18:09:57 +0700

    feature/#972/homePage_task

--- my-app/src/components/Slide.jsx
+++ my-app/src/components/Slide.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import { useEffect , useState } from "react";
 function Slide() {
+    
     return ( 
         <>
-          
+          <div className="slide-show">
+
+          </div>
         </>
      );
 }
