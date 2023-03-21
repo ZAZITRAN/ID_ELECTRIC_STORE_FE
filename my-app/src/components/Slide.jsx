2709fa097db85d2beeba927e23061e8bea45f0d9
LeDuc2004 <114064159+LeDuc2004@users.noreply.github.com>
2023-03-21 11:14:51 +0700

    update

--- my-app/src/components/Slide.jsx
+++ my-app/src/components/Slide.jsx
@@ -1,12 +1,61 @@
 import React from "react";
-import { useEffect , useState } from "react";
+import "./slide.scss"
+
 function Slide() {
     
     return ( 
         <>
-          <div className="slide-show">
+      <div class="slide-container">
 
-          </div>
+       <div class="btn-cha">
+
+       <div class="slide-btn left">
+       <i class="fa-solid fa-chevron-left"></i>
+       </div>
+
+    <div class="slide-btn right">
+      <i class="fa-solid fa-chevron-right"></i>
+      </div>
+
+      </div>
+
+      <div class="slide-show">
+
+           <div class="current-img">
+          <div class="current-img cr"></div>
+          <div class="current-img"></div>
+          <div class="current-img"></div>
+          <div class="current-img"></div>
+        </div>
+        <div class="slide-run" >
+           <img src="https://firebasestorage.googleapis.com/v0/b/uploadimg-a9a88.appspot.com/o/image%2Fimgbnb%20(4).jpeg?alt=media&token=a98bf1fa-c525-4841-a5b2-4fed3e909283" alt="" />
+           <img src="https://firebasestorage.googleapis.com/v0/b/uploadimg-a9a88.appspot.com/o/image%2Fimgbnb%20(4).jpeg?alt=media&token=a98bf1fa-c525-4841-a5b2-4fed3e909283" alt="" />
+           <img src="https://firebasestorage.googleapis.com/v0/b/uploadimg-a9a88.appspot.com/o/image%2Fimgbnb%20(4).jpeg?alt=media&token=a98bf1fa-c525-4841-a5b2-4fed3e909283" alt="" />
+           <img src="https://firebasestorage.googleapis.com/v0/b/uploadimg-a9a88.appspot.com/o/image%2Fimgbnb%20(4).jpeg?alt=media&token=a98bf1fa-c525-4841-a5b2-4fed3e909283" alt="" />
+
+        </div>
+      </div>
+  </div>
+
+
+ 
+
+
+
+
+  <div class="category">
+
+    <div class="category__text">Category</div>
+
+    <div class="category__list">
+
+      <div class="category__sun">
+            
+          <div class="sun-text">Computer Laptop</div>
+      </div>
+          
+    </div>
+    </div>
         </>
      );
 }
